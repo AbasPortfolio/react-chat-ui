494f603637ab83ed8d5894594207b3f5a08cae52
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-04-23 14:24:02 +0200

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-nu-rouge.vercel.app/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-a0h8.onrender.com/api" : "/api",
   withCredentials: true,
 });
